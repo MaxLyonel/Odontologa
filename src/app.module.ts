38d00a47a3989ae331b48370cad23dc43965f16f
Leonel Maximo Vargas Ramirez <maxramirez@MacBook-Pro-de-Leonel.local>
2023-09-23 19:39:02 -0400

    Configuracion inicial del proyecto

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,34 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import dataBaseConfig from './config/database.config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 @Module({
-  imports: [],
+  // El metodo forRoot registra el proveedor ConfigService, que proporciona un metodo get()
+  // para leer las variables de configuracion
+  imports: [ConfigModule.forRoot({
+      isGlobal: true,
+      load: [ dataBaseConfig ]
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get<string>('DATABASE_HOST'),
+        port: configService.get<number>('DATABASE_PORT'),
+        username: configService.get<string>('DATABASE_USERNAME'),
+        password: configService.get<string>('DATABASE_PASSWORD'),
+        database: configService.get<string>('DATABASE_NAME'),
+        entities: [],
+        synchronize: true,
+      }),
+    })
+  ], // con esto cargamos las variables de entorno
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService], // los proveedores que seran instanciados por el inyector de Nest y que pueden compartirse al menos en este modulo
 })
-export class AppModule {}
+export class AppModule {
+  constructor(private configService: ConfigService){}
+}
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,18 @@
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class AppService {
+  constructor(private configService: ConfigService) {}
   getHello(): string {
-    return 'Hello World!';
+
+    interface DatabaseConfig {
+      host: string,
+      port: number
+    }
+    const dbConfig = this.configService.get<DatabaseConfig>('database');
+    // const dbUser = this.configService.get<string>('DATABASE_USER');
+    // return dbConfig.port + ' ' + dbConfig.host ;
+    return this.configService.get<string>('database.user', 'leonel');
   }
 }
